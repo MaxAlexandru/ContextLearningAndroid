2a0e240f1fa4ef7f2b4a512274a089782099c597
Max <max.alexandru93@gmail.com>
2017-05-30 16:02:20 +0300

    Database view + location service.

--- app/src/main/java/com/max/app/contextlearning/SensorService.java
+++ app/src/main/java/com/max/app/contextlearning/SensorService.java
@@ -1,33 +1,48 @@
-package com.example.max.contextlearning;
+package com.max.app.contextlearning;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
-/**
- * Created by Max on 3/6/2017.
- */
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
-public class VolumeService extends Service {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SensorService extends Service implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     /* Sensor Listener Class. */
     private class MySensorListener implements SensorEventListener {
 
+        private final int last = 5;
         private int type;
+        private ArrayList<Float> light;
 
         private MySensorListener(int type) {
             this.type = type;
+            this.light = new ArrayList<>();
         }
 
         @Override
@@ -36,8 +51,15 @@ public class VolumeService extends Service {
             Intent intent = new Intent();
             switch (type) {
                 case Sensor.TYPE_LIGHT:
-                    value = sensorEvent.values[0] + "";
-                    Log.i("SensorService", value);
+                    if (light.size() > last)
+                        light.remove(0);
+                    light.add(sensorEvent.values[0]);
+                    float average = 0;
+                    for (float i : light)
+                        average += i;
+                    average /= light.size();
+                    value = String.valueOf(average);
+                    Log.i(Constants.TAG, value);
                     intent.setAction(Constants.ACTIONS.get("Light"));
                     intent.putExtra("LightSensor", value);
                     break;
@@ -68,8 +90,39 @@ public class VolumeService extends Service {
         }
 
         @Override
-        public void onAccuracyChanged(Sensor sensor, int i) { }
+        public void onAccuracyChanged(Sensor sensor, int i) {
+        }
     }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        lastLocation = mLastLocation.getLatitude() + " " + mLastLocation.getLongitude();
+
+        Log.i(Constants.TAG, "[SensorService]: " +
+                mLastLocation.getLatitude() + " " + mLastLocation.getLongitude());
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {}
 
     public SensorManager mySensorManager;
     public MySensorListener lightListener;
@@ -82,8 +135,11 @@ public class VolumeService extends Service {
     private final String TAG = "VOLUME SERVICE";
     private final int SAMPLE_RATE = 44100;
     private Runnable r;
+    private Thread thread;
+    public GoogleApiClient mGoogleApiClient;
+    public String lastLocation;
 
-    public VolumeService() {
+    public SensorService() {
         lightListener = new MySensorListener(Sensor.TYPE_LIGHT);
         proximityListener = new MySensorListener(Sensor.TYPE_PROXIMITY);
         gravityListener = new MySensorListener(Sensor.TYPE_GRAVITY);
@@ -93,7 +149,15 @@ public class VolumeService extends Service {
     @Override
     public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
-        Log.i(TAG, "Service has started");
+        Log.i(Constants.TAG, "[SensorService]: onStartCommand()");
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+        mGoogleApiClient.disconnect();
 
         mySensorManager = (SensorManager) getSystemService(android.content.Context.SENSOR_SERVICE);
         sharedPref = getSharedPreferences(
@@ -106,8 +170,11 @@ public class VolumeService extends Service {
                 private int prevSensorsStatus;
 
                 public void run() {
+                    Log.i(Constants.TAG, "[SensorServiceRunnable]: run()");
+
                     /* Registering sensors listeners. */
                     sensorsServiceStatus = sharedPref.getString("SensorsService", null);
+                    Log.i(Constants.TAG, "[SensorServiceRunnable]: " + sensorsServiceStatus);
                     prevSensorsStatus = 0;
                     if (sensorsServiceStatus != null && sensorsServiceStatus.equals("On")) {
                         prevSensorsStatus = 1;
@@ -153,11 +220,16 @@ public class VolumeService extends Service {
                         noiseIntent.setAction(Constants.ACTIONS.get("Noise"));
                         noiseIntent.putExtra("NoiseSensor", db);
                         sendBroadcast(noiseIntent);
+
+                        Intent locationIntent = new Intent();
+                        locationIntent.setAction(Constants.ACTIONS.get("Location"));
+                        locationIntent.putExtra("LocationSensor", lastLocation);
+                        sendBroadcast(locationIntent);
                     }
                 }
             };
-            Thread volThread = new Thread(r);
-            volThread.start();
+            thread = new Thread(r);
+            thread.start();
         }
         return Service.START_STICKY;
     }
@@ -190,6 +262,7 @@ public class VolumeService extends Service {
                 AudioFormat.ENCODING_PCM_16BIT,
                 buffSize);
         audioRecord.startRecording();
+        mGoogleApiClient.connect();
     }
 
     public void unregisterSensors() {
@@ -203,6 +276,7 @@ public class VolumeService extends Service {
         } catch (Exception e) {
             Log.e(TAG, e.toString());
         }
+        mGoogleApiClient.disconnect();
     }
 
     @Nullable
